28a7df4d968a6b7664643592a59f019b1d01a0f0
Karen Stevenson <karen@karen-stevenson.com>
2017-05-29 06:14:10 -0500

    Fix critical error in interface

--- src/SchemaMetatagManagerInterface.php
+++ src/SchemaMetatagManagerInterface.php
@@ -9,22 +9,22 @@ namespace Drupal\schema_metatag;
  */
 interface SchemaMetatagManagerInterface {
 
-  /**
+ /**
    * Convert a metatags-style data array to JSON LD.
    *
    * Multiple groups can be combined under @graph.
    * All groups that have properties filled out will be presented here.
    */
-  static function jsonld($metatag_data, $route_entity = NULL) {}
+  static function jsonld( $metatag_data, $route_entity);
 
    /**
    * Utility function to get a single group.
    */
-  static function groups($group_id = NULL) {}
+  static function groups( $group_id);
 
   /**
    * Utility function to get groups that implement the Schema.org definitions.
    */
-  static function all_groups() {}
+  static function all_groups();
 
 }
