b6ed5303f6120d509d72be58f6c96221f6c2e7f8
karens <karens@45874.no-reply.drupal.org>
2018-05-25 11:55:34 -0500

    Issue #2972981 by KarenS, rommaz: Add ContactPoint property for Organization and Person

--- src/SchemaPlaceTrait.php
+++ src/SchemaPlaceTrait.php
@@ -57,6 +57,7 @@ trait SchemaPlaceTrait {
       '#empty_value' => '',
       '#options' => [
         'Place' => $this->t('Place'),
+        'AdministrativeArea' => $this->t('AdministrativeArea'),
       ],
       '#required' => $input_values['#required'],
       '#weight' => -10,
